347a1fcfa200362a29aaa4506e24e28676f2a24b
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2022-07-03 19:14:56 -0700

    Added heap using built-in container/heap package

---
+++ collections/heap_test.go
@@ -0,0 +1,28 @@
+package collections
+
+import (
+	"practice/constraints"
+	"practice/test"
+	"testing"
+)
+
+func TestHeap(t *testing.T) {
+	heap := NewHeap[constraints.OrderedInt]()
+	params := test.DefaultParams
+	for i := params.RangeStart; i < params.RangeEnd; i++ {
+		heap.Cap()
+		if heap.Len() != i-params.RangeStart {
+			t.Errorf("Heap shows length %d, should be %d\n", heap.Len(), i)
+		}
+		heap.Push(constraints.OrderedInt(i))
+	}
+
+	length := params.RangeEnd - params.RangeStart
+	for i := length - 1; i >= 0; i-- {
+		peek, ok1 := heap.Peek()
+		pop, ok2 := heap.Pop()
+		if !ok1 || !ok2 || heap.Len() != i || peek != pop {
+			t.Errorf("Peek ok? %t\nPop ok? %t\nLength (should be %d)? %d\nPeek val? %d\nPop val? %d\n", ok1, ok2, i, heap.Len(), peek, pop)
+		}
+	}
+}
